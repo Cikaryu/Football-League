8bab958f45fdc4646605d3ab4cee2b89775e18a0
Cikaryu <131530876+Cikaryu@users.noreply.github.com>
2024-01-05 17:41:07 +0800

    Update ApiInterface.java

--- app/src/main/java/com/uts/uasmobprogug214/ApiInterface.java
+++ app/src/main/java/com/uts/uasmobprogug214/ApiInterface.java
@@ -5,13 +5,14 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface ApiInterface {
-
+    @GET("sport/sport-api/leaguesList")
+    Call<ApiResponse> getResults();
     @GET("sport/sport-api/results")
     Call<ApiResponse> getResults();
 
     @GET("sport/sport-api/league")
     Call<ApiResponse> getLeagues();
 
-    @GET("sport/sport-api/goalkings")
+    @GET("sport/sport-api/goalKings")
     Call<ApiResponse> getGoalKings();
 }
