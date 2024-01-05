b9c32dd001902ae0dfda0ef64e5fb35cf4232bd8
Cikaryu <131530876+Cikaryu@users.noreply.github.com>
2024-01-05 17:18:56 +0800

    api

---
+++ app/src/main/java/com/uts/uasmobprogug214/ApiInterface.java
@@ -0,0 +1,17 @@
+package com.uts.uasmobprogug214;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+
+    @GET("sport/sport-api/results")
+    Call<ApiResponse> getResults();
+
+    @GET("sport/sport-api/league")
+    Call<ApiResponse> getLeagues();
+
+    @GET("sport/sport-api/goalkings")
+    Call<ApiResponse> getGoalKings();
+}
