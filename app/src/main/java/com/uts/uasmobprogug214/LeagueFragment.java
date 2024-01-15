0599ef773f4499c9e9127c532e0e0703d508c4fa
Gung Satria <131530876+Cikaryu@users.noreply.github.com>
2024-01-15 17:08:48 +0800

    frag baru

--- app/src/main/java/com/uts/uasmobprogug214/LeagueFragment.java
+++ app/src/main/java/com/uts/uasmobprogug214/LeagueFragment.java
@@ -1,10 +1,14 @@
 package com.uts.uasmobprogug214;
 
 import static android.widget.Toast.LENGTH_LONG;
-import static android.widget.Toast.LENGTH_SHORT;
 
 import android.content.Context;
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,21 +17,21 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import com.uts.uasmobprogug214.models.ModelLeague;
 import com.uts.uasmobprogug214.models.ResultLeague;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-import java.util.ArrayList;
-import java.util.List;
-
-
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LeagueFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class LeagueFragment extends Fragment {
     Context ctx;
     Button btn1;
@@ -38,27 +42,65 @@ public class LeagueFragment extends Fragment {
     List<ModelLeague> data1;
     Spinner spinteam, spinleague;
 
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public LeagueFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LeagueFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LeagueFragment newInstance(String param1, String param2) {
+        LeagueFragment fragment = new LeagueFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_league, container, false);
         recyclerView = view.findViewById(R.id.recyclerView);
         spinteam = view.findViewById(R.id.spinTeam);
         spinleague = view.findViewById(R.id.spinLeague);
-        return view;
 
         ctx = getActivity();
-        ArrayAdapter<CharSequence> leaguesAdapter = ArrayAdapter.createFromResource(
+        /*ArrayAdapter<CharSequence> leaguesAdapter = ArrayAdapter.createFromResource(
                 this,
                 R.array.leagues,
                 android.R.layout.simple_spinner_item);
         leaguesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinleague.setAdapter(leaguesAdapter);
-
+        */
         ArrayAdapter<CharSequence> teamsAdapter = ArrayAdapter.createFromResource(
-                this,
+                ctx,
                 R.array.teams,
-                android.R.layout.simple_spinner_item
-        );
+                android.R.layout.simple_spinner_item);
         teamsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinteam.setAdapter(teamsAdapter);
 
